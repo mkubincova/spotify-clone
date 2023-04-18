f8b2c530dbcdcb602c9e8f770e935bb4b9758415
magda <magdalena.kubincova@protonmail.com>
2023-04-18 03:38:51 +0200

    user menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,12 @@
-<script>
+<script lang="ts">
+	import { tippy } from '$actions';
 	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import { LogoutButton } from '$components';
 	import Navigation from './Navigation.svelte';
+	import { ChevronDown, ExternalLink } from 'lucide-svelte';
+
+	$: user = $page.data.user;
 </script>
 
 <div class="content">
@@ -9,7 +15,49 @@
 			<Navigation desktop={false} />
 		{/if}
 	</div>
-	<div class="right" />
+	<div class="right">
+		<div
+			id="profile-button"
+			use:tippy={{
+				content: document.getElementById('profile-menu') || undefined,
+				onMount: () => {
+					const template = document.getElementById('profile-menu');
+					if (template) {
+						template.style.display = 'block';
+					}
+				},
+				trigger: 'click',
+				placement: 'bottom-end',
+				interactive: true,
+				theme: 'menu'
+			}}
+		>
+			<button class="profile-button" aria-label="Profile menu">
+				{#if user?.images && user.images.length > 0}
+					<img src={user.images[0].url} alt="" />
+				{/if}
+				{user?.display_name}
+				<ChevronDown class="profile-arrow" size={22} />
+			</button>
+		</div>
+		<div id="profile-menu" style="display: none;">
+			<div class="profile-menu-content">
+				<ul>
+					<li>
+						<a href={user?.external_urls.spotify} target="_blank" rel="noopener noreferer"
+							>View on Spotify <ExternalLink focusable="false" aria-hidden size={20} /></a
+						>
+					</li>
+					<li>
+						<a href="/profile">View Profile</a>
+					</li>
+					<li>
+						<LogoutButton />
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
@@ -18,5 +66,59 @@
 		justify-content: space-between;
 		align-items: center;
 		width: 100%;
+	}
+	.profile-button {
+		background: none;
+		border: 1px solid var(--border);
+		padding: 5px;
+		border-radius: 25px;
+		display: flex;
+		align-items: center;
+		color: var(--text-color);
+		cursor: pointer;
+
+		&:hover {
+			background-color: var(--accent-color);
+		}
+		:global(.profile-arrow) {
+			margin-left: 3px;
+		}
+		img {
+			width: 28px;
+			aspect-ratio: 1;
+			border-radius: 100%;
+			margin-right: 10px;
+		}
+	}
+	.profile-menu-content {
+		padding: 5px 0;
+		ul {
+			padding: 0;
+			margin: 0;
+			list-style: none;
+
+			li {
+				&:hover {
+					background-image: linear-gradient(rgba(255, 255, 255, 0.07) 0 0);
+				}
+				a :global(svg) {
+					vertical-align: middle;
+					margin-left: 10px;
+				}
+				a,
+				:global(button) {
+					padding: 10px 15px;
+					display: inline-block;
+					background: transparent;
+					border: none;
+					text-decoration: none;
+					cursor: pointer;
+					color: var(--menu-text-color);
+					width: 100%;
+					text-align: left;
+					font-size: functions.toRem(14);
+				}
+			}
+		}
 	}
 </style>
